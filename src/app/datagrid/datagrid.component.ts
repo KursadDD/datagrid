db9cac3e08d37aa0d75f3c6df72a6c4d1c373fd8
kursad <kursad.demirdogen@gmail.com>
2023-09-25 08:24:11 +0300

    menülere link verildi

--- src/app/datagrid/datagrid.component.ts
+++ src/app/datagrid/datagrid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DatagridServiceService } from 'src/services/datagrid-service.service';
+import { DatagridServiceService } from 'src/app/services/datagrid-service.service';
 
 @Component({
   selector: 'app-datagrid',
